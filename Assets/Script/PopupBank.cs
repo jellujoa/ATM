f65ed7e7a566cc5d215edfc535a8c7b0d1e0f7c8
jellujoa <jellujoa03@gmail.com>
2025-06-04 17:57:16 +0900

    step 7

--- Assets/Script/PopupBank.cs
+++ Assets/Script/PopupBank.cs
@@ -7,24 +7,41 @@ public class PopupBank : MonoBehaviour
 {
     public Button Deposit;
     public Button Withdraw;
+    public Button Back;
+    public Button Back2;
+    
+    public GameObject ATM;
+    public GameObject DepositUI;  
+    public GameObject WithdrawUI;
     
     public void Start()
-    {
-        // 버튼 클릭 이벤트 등록
+    {      
+        Back.onClick.AddListener(OnBackButtonClick);
+        Back2.onClick.AddListener(OnBackButtonClick);
         Deposit.onClick.AddListener(OnDepositButtonClick);
         Withdraw.onClick.AddListener(OnWithdrawButtonClick);
     }
     public void OnDepositButtonClick()
     {
-       
+       ATM.SetActive(false);
+       DepositUI.SetActive(true);
     }
     public void OnWithdrawButtonClick()
     {
-
+        ATM.SetActive(false);
+        WithdrawUI.SetActive(true);
     }
-    // Update is called once per frame
-    void Update()
+    public void OnBackButtonClick()
     {
-        
+        ATM.SetActive(true);
+        DepositUI.SetActive(false);
+        WithdrawUI.SetActive(false);
     }
+    public void OnBack2ButtonClick()
+    {
+        ATM.SetActive(true);
+        DepositUI.SetActive(false);
+        WithdrawUI.SetActive(false);
+    }
+    
 }
