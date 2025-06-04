34e5d7f66813b58abe0c8ce6638c9218268fe9d1
jellujoa <jellujoa03@gmail.com>
2025-06-04 16:11:39 +0900

    step 4

---
+++ Assets/Script/GameManager.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;   
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager Instance;
+
+    public UserData userData;
+
+    public Text userNameText;
+    public Text cashText;
+    public Text bankBalanceText;
+
+    private void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+        }
+
+    }
+    public void Start()
+    {
+        userData = new UserData("이찬", 100000, 50000);
+        userData.userNameText = userNameText;
+        userData.cashText = cashText;
+        userData.bankBalanceText = bankBalanceText;
+
+         userData.RefreshUI();
+        // UI 갱신
+
+    }
+    public void Update()
+    {
+        userData.RefreshUI();
+    }
+
+}
---
+++ Assets/Script/UserData.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+[System.Serializable]
+public class UserData 
+{
+    
+    public string userName;
+    public int cash;
+    public int bankBalance;
+
+    [HideInInspector]
+    public Text userNameText;
+    [HideInInspector]
+    public Text cashText;
+    [HideInInspector]
+    public Text bankBalanceText;
+
+    public UserData(string userName, int cash, int bankBalance)
+    {
+        this.userName = userName;
+        this.cash = cash;
+        this.bankBalance = bankBalance;
+    }
+
+    public void RefreshUI() 
+    {
+        if (userNameText != null)
+            userNameText.text =  userName;
+        if (cashText != null)
+            cashText.text = cash.ToString("N0"); // 쉼표 포함
+        if (bankBalanceText != null)
+            bankBalanceText.text = bankBalance.ToString("N0");
+    }
+}
