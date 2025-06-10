cf33faa6da54b81be8c43f7807a2c35799492a6b
jellujoa <jellujoa03@gmail.com>
2025-06-10 17:18:39 +0900

    10

--- Assets/Script/GameManager.cs
+++ Assets/Script/GameManager.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.IO;
 using UnityEngine;
 using UnityEngine.UI;   
 
@@ -15,17 +16,35 @@ public class GameManager : MonoBehaviour
 
     public GameObject Error;
 
+    private string filePath;
+
     private void Awake()
     {
         if (Instance == null)
         {
             Instance = this;
         }
+        string directoryPath = Application.persistentDataPath + "/Saves"; // Saves 폴더 경로
+        if (!Directory.Exists(directoryPath)) // 폴더가 없으면 생성
+        {
+            Directory.CreateDirectory(directoryPath);
+        }
 
+        filePath = directoryPath + "/userData.json"; // 저장할 파일 경로
     }
+
     public void Start()
     {
-        userData = new UserData("이찬", 100000, 50000);
+        if (File.Exists(filePath))
+        {
+            LoadUserData();
+        }
+        else
+        {
+            userData = new UserData("이찬", 100000, 50000);
+        }
+
+       
         userNameText.text = userData.userName;
         cashText.text = userData.cash.ToString("N0"); // 쉼표 포함
         bankBalanceText.text = userData.bankBalance.ToString("N0");
@@ -54,6 +73,7 @@ public class GameManager : MonoBehaviour
             userData.cash -= amount;
             userData.bankBalance += amount;
             RefreshUI();
+            SaveUserData();
         }
         else
         {
@@ -67,11 +87,25 @@ public class GameManager : MonoBehaviour
             userData.bankBalance -= amount;  // 은행 잔액 차감
             userData.cash += amount;  // 현금 추가
             RefreshUI();  // UI 갱신
+            SaveUserData();
         }
         else
         {
             Error.SetActive(true); // 잔액 부족 시 에러 메시지 표시
         }
     }
+    public void SaveUserData()
+    {
+        string json = JsonUtility.ToJson(userData);
+        File.WriteAllText(filePath, json);
+       
+    }
 
+    // JSON 파일에서 UserData를 불러오는 메서드
+    public void LoadUserData()
+    {
+        string json = File.ReadAllText(filePath);
+        userData = JsonUtility.FromJson<UserData>(json);
+      
+    }
 }
